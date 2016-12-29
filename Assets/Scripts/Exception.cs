7e583a5fe47e549cdeae24b45ef91f8a19bf10a2
chameleon <martinkro@aliyun.com>
2016-12-29 21:12:56 +0800

    Exception Demo

---
+++ Assets/Scripts/Exception.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exception : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Exception Start ...");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		if (GUILayout.Button ("Press Me")) {
+			Debug.Log ("Hello!");
+		}
+		
+	}
+}
---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManagerScript : MonoBehaviour {
+
+	public void ExceptionDivideByZero()
+	{
+		Debug.Log ("Exception:Divide By Zero");
+	}
+
+	public void ExceptionNullReferences()
+	{
+		Debug.Log ("Exception:Null References");
+	}
+}
